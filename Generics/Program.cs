68d4cd6ad20a533d9c4efa79dc44cd208e689b75
Mohanraj209 <mohanraj1715@gmail.com>
2022-03-20 12:32:01 +0530

    Displaying Message

---
+++ Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(" ---------** Test Maximum of Three Variables **--------- ");
+        }
+    }
+}
